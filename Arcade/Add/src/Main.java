11beef4ea7b2e901b54cc816226afcc29855839f
Anu Tomy <anuatomy@gmail.com>
2018-03-01 12:29:21 -0500

    Initial commit

---
+++ Arcade/Add/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        //Write a function that returns the sum of two numbers.
+        int param1 = 3;
+        int param2 = 2;
+        System.out.println(add(param1,param2));
+    }
+
+    public static int add(int num1, int num2){
+        return num1 + num2;
+    }
+}
