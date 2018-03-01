7e9a71f6b229fadc863adaa58fafd377683d1d6e
Anu Tomy <anuatomy@gmail.com>
2018-03-01 12:36:17 -0500

    Create a function that returns sum of two numbers

--- Arcade/Add/src/Main.java
+++ Arcade/Add/src/Main.java
@@ -6,8 +6,8 @@ public class Main {
         int param2 = 2;
         System.out.println(add(param1,param2));
     }
-
     public static int add(int num1, int num2){
+
         return num1 + num2;
     }
 }
